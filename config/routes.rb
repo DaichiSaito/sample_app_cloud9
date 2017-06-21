83a6d0b6aebabf797889ef4abd27cc39439c078d
Daichi Saito <hrkedz@gmail.com>
2017-06-21 20:40:47 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  get 'users/new'
+  # get 'users/new'
 
   root 'static_pages#home'
   
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',   to: 'users#new'
+  
+  resources :users
 end
