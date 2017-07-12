b3cd3fcb36494eb38ddcd84c61b027f7b77a9f31
Daichi Saito <hrkedz@gmail.com>
2017-07-12 01:51:55 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user # ヘルパーメソッド
-       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
+      # redirect_to user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
